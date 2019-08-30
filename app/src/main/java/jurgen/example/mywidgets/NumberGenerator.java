894b3bedf92d049bbf9de843b62fd739e5771977
Jurgen <jurgenirgof@gmail.com>
2019-08-30 20:57:05 +0700

    Initial commit

---
+++ app/src/main/java/jurgen/example/mywidgets/NumberGenerator.java
@@ -0,0 +1,11 @@
+package jurgen.example.mywidgets;
+
+import java.util.Random;
+
+public class NumberGenerator {
+
+    public static int Generate(int max) {
+        Random random = new Random();
+        return random.nextInt(max);
+    }
+}
